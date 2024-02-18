c0089adb348f8e559e81ff7afad95e9365226a82
Ghoulgager <bagramyan2000@gmail.com>
2024-02-18 20:25:53 +0100

    part2 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,35 +1,138 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import personService from './services/persons'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import Filter from './components/Filter'
+import Notification from './components/Notification'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import './index.css'
+
+const App = () => {
+    const [ persons, setPersons] = useState([])
+    const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
+    const [ newFilter, setNewFilter ] = useState('')
+    const [ notificationMessage, setNotificationMessage] = useState(null)
+
+    const personsToShow = newFilter.length === 0 ? persons
+                            : persons.filter(person => person.name.search(newFilter) >= 0)
+
+    useEffect(() => {
+        const dataHook = () => {
+            personService
+                .getAll()
+                .then(initialPersons => {
+                    setPersons(initialPersons)
+                })
+        }
+
+        dataHook()
+    }, []);
+
+    const addContact = (e) => {
+        e.preventDefault();
+
+        const newPersonObject = {
+            name: newName,
+            number: newNumber
+        }
+
+        const msg = `${ newName } is already added to phonebook, replace the old number with a new one?`
+        const duplicatePerson = persons.find(p => p.name === newName);
+
+        // using truthy to check if duplicate is found
+        if (duplicatePerson) {
+            if (window.confirm(msg) === true) {
+                personService
+                    .update(duplicatePerson.id, newPersonObject)
+                    .then(returnedPerson => {
+
+                        setNotificationMessage({
+                            "text": `Updated ${ returnedPerson.name }`,
+                            "type": "notification"
+                        })
+
+                        setTimeout(() => {
+                            setNotificationMessage(null)
+                        }, 5000)
+
+                        setPersons(persons.map(p => p.id !== duplicatePerson.id ? p : returnedPerson))
+                    })
+            }
+        } else {
+            personService
+                .create(newPersonObject)
+                .then(returnedPerson => {
+
+                    setNotificationMessage({
+                        "text": `Added ${ returnedPerson.name }`,
+                        "type": "notification"
+                    })
+
+                    setTimeout(() => {
+                        setNotificationMessage(null)
+                    }, 5000)
+
+                    setPersons(persons.concat(returnedPerson))
+                })
+        }
+    }
+
+    const destroyContact = (e) => {
+        const id = Number(e.target.id)
+        const name = e.target.name
+        const msg = `Do you really want to delete ${ name }?`
+
+        if (window.confirm(msg) === true) {
+            personService
+                .destroy(id)
+                .then(destroyedPerson => {
+                    setPersons(persons.filter(p => p.id !== id))
+                })
+                .catch(error => {
+                    setNotificationMessage({
+                        "text": `The person, ${ name } was already removed from server`,
+                        "type": "error"
+                    })
+
+                    setTimeout(() => {
+                        setNotificationMessage(null)
+                    }, 5000)
+
+                    setPersons(persons.filter(p => p.id !== id))
+                })
+        }
+    }
+
+    const handleNameChange = (e) => setNewName(e.target.value)
+    const handleNumberChange = (e) => setNewNumber(e.target.value)
+    const handleFilterChange = (e) => setNewFilter(new RegExp(e.target.value, 'ig'))
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+
+            { notificationMessage !== null ? <Notification message={ notificationMessage } /> : null }
+
+            <Filter
+                handleFilterChange={ (e) => { handleFilterChange(e) } }
+            />
+
+            <h2>Add a new</h2>
+            <PersonForm
+                addContact={ (e) => addContact(e) }
+                handleNameChange={ (e) => handleNameChange(e) }
+                handleNumberChange= { (e) => handleNumberChange(e) }
+            />
+
+            <h2>Numbers</h2>
+            <Persons
+                persons={ personsToShow }
+                destroyContact={ (e) => destroyContact(e) }
+            />
+        </div>
+    )
 }
 
 export default App
